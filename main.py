b2f7a8e140f879942be11bfcd3fc0fb15698a3c3
taylormeador <taylor.r.meador@gmail.com>
2022-08-04 18:18:06 -0600

    begin implementing game configuration options

--- main.py
+++ main.py
@@ -7,6 +7,71 @@ import setup
 import globals
 
 
+# loop that allows player to choose settings before beginning game
+def config():
+    p.init()
+    p.display.set_caption('Chess 9000')
+    screen = p.display.set_mode((globals.BOARD_WIDTH, globals.BOARD_HEIGHT))
+    game_config = setup.Config()
+    start_button_enable = False
+    running = True
+    while running:
+        screen.fill(p.Color("black"))
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+
+            # mouse handler
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (x, y) of mouse click position
+                for button in setup.buttons:
+                    if button.button_rect.collidepoint(location):  # press the button
+                        button.selected = not button.selected
+                        for time_button in setup.time_buttons:  # deselect other time control buttons
+                            if button in setup.time_buttons and button != time_button:
+                                time_button.selected = False
+                        for color_button in setup.piece_color_buttons:  # deselect other color button
+                            if button in setup.piece_color_buttons and button != color_button:
+                                color_button.selected = False
+                        for light_color_button in setup.light_color_buttons:  # deselect other color button
+                            if button in setup.light_color_buttons and button != light_color_button:
+                                light_color_button.selected = False
+                        for dark_color_button in setup.dark_color_buttons:  # deselect other color button
+                            if button in setup.dark_color_buttons and button != dark_color_button:
+                                dark_color_button.selected = False
+                        if not start_button_enable:  # don't allow game to start until valid choices are made
+                            setup.start_button.selected = False
+
+        # draw buttons
+        for button in setup.buttons:
+            button.draw_button(screen)
+
+        # don't let user start game without setting up
+        if (setup.white_select_button.selected or setup.black_select_button.selected) and \
+                (setup.time_3_0_button.selected or setup.time_3_2_button.selected or setup.time_5_0_button.selected):
+            start_button_enable = True
+
+        # set selected config vars
+        game_config.white_is_human = True if setup.white_select_button.selected else False
+        game_config.black_is_human = True if setup.black_select_button.selected else False
+        game_config.time_control = 300 if setup.time_5_0_button.selected else 180
+        game_config.increment = 2 if setup.time_3_2_button.selected else 0
+        for button in setup.light_color_buttons:
+            if button.selected:
+                game_config.light_square_color = p.Color(button.button_text)
+        for button in setup.dark_color_buttons:
+            if button.selected:
+                game_config.dark_square_color = p.Color(button.button_text)
+
+        # call the main game loop once the start button is enabled and pressed
+        if setup.start_button.selected:
+            game_config.set_config()
+            main()
+
+        p.display.flip()
+    main()
+
+
 # main function contains setup and game loop
 def main():
     p.init()
@@ -38,7 +103,9 @@ def main():
                     player_clicks = []  # clear player_clicks
                 else:  # if the player clicks on a piece or empty square
                     square_selected = gs.board[row][col]
-                    if len(player_clicks) == 1:  # always append second click
+                    # append second click if it's not player's own piece
+                    if len(player_clicks) == 1 and (square_selected.piece not in globals.FEN_PIECES[0:6] and gs.color_to_move == "w") or \
+                            (square_selected.piece not in globals.FEN_PIECES[6:12] and gs.color_to_move == "b"):
                         player_clicks.append(square_selected)
                     # only want to count the click if it's on an ally piece
                     if (square_selected.piece in globals.FEN_PIECES[0:6] and gs.color_to_move == "w") or \
@@ -61,4 +128,4 @@ def main():
     sys.exit()
 
 
-main()
+config()
--- setup.py
+++ setup.py
@@ -1,9 +1,68 @@
 import pygame as p
 import globals
 
-
+# loads images into dict
 def load_images(piece_set):
     pieces = ["wP", "wN", "wB", "wR", "wQ", "wK", "bP", "bN", "bB", "bR", "bQ", "bK"]
     for piece in pieces:
         path = "./images/" + piece_set + "/" + piece + ".png"
         globals.IMAGES[piece] = p.transform.scale(p.image.load(path), (globals.SQ_SIZE, globals.SQ_SIZE))
+
+# custom class for storing game config variables
+class Config:
+    def __init__(self):
+        self.white_is_human = None
+        self.black_is_human = None
+        self.light_square_color = None
+        self.dark_square_color = None
+        self.time_control = None
+        self.increment = None
+
+    def set_config(self):
+        globals.WHITE_POV = self.white_is_human
+        globals.LIGHT_SQUARE_COLOR = self.light_square_color
+        globals.DARK_SQUARE_COLOR = self.dark_square_color
+        globals.TIME_CONTROL = self.time_control
+        globals.INCREMENT = self.increment
+
+
+class Button:
+    def __init__(self, button_text, button_location, button_width):
+        self.button_text = button_text
+        self.button_location = button_location
+        self.button_width = button_width
+        self.button_height = 35
+        self.button_rect = p.Rect(self.button_location[0], self.button_location[1], self.button_width, self.button_height)
+        self.selected = False
+        self.selected_color = p.Color("cyan")
+
+    def draw_button(self, screen):
+        # draw the rectangle
+        color = self.selected_color if self.selected else p.Color("white")
+        p.draw.rect(screen, color, self.button_rect)
+
+        # draw the text
+        font = p.font.SysFont("Helvetica", 20, False, False)
+        text_object = font.render(self.button_text, 1, p.Color("Black"))
+        text_location = (self.button_location[0] + self.button_width//6, self.button_location[1] + self.button_height//6)
+        screen.blit(text_object, text_location)
+
+
+start_button = Button("Start Game", (globals.BOARD_WIDTH - 200, globals.BOARD_HEIGHT - 50), 150)
+white_select_button = Button("White", (25, 25), 75)
+black_select_button = Button("Black", (25, 75), 75)
+time_3_0_button = Button("3+0", (150, 25), 60)
+time_3_2_button = Button("3+2", (150, 75), 60)
+time_5_0_button = Button("5+0", (150, 125), 60)
+white_button = Button("White", (250, 25), 85)
+beige_button = Button("Beige", (250, 75), 85)
+ivory_button = Button("Ivory", (250, 125), 85)
+gray_button = Button("Gray", (350, 25), 85)
+dark_gray_button = Button("Dark Gray", (350, 75), 125)
+green_button = Button("Dark Green", (350, 125), 150)
+
+buttons = [start_button, white_select_button, black_select_button, time_3_0_button, time_3_2_button, time_5_0_button, white_button, beige_button, ivory_button, gray_button, dark_gray_button, green_button]
+time_buttons = [time_3_0_button, time_3_2_button, time_5_0_button]
+piece_color_buttons = [white_select_button, black_select_button]
+light_color_buttons = [white_button, beige_button, ivory_button]
+dark_color_buttons = [gray_button, dark_gray_button, green_button]
